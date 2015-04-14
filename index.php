d33d6703c3ccf23c6d22cf24020624d5f0b79ad9
TheAndreiM <andreifundrei@gmail.com>
2015-04-13 22:57:58 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<script src="juliar.js"></script>
+<juliar>*subscript 2 * *superscript yay * x hello *bold yay * *larger boo* add 5 5 *add 2 4 * 2 *add 3 5 * </juliar>
+
+<ijuliar></ijuliar>
