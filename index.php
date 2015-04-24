79a9b36c5f10e6a28b5686b3661add81969dfa99
TheAndreiM <andreifundrei@gmail.com>
2015-04-23 21:57:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 *background pink *
 *rainbow=red,orange,green,blue,purple Welcome *ask What's your name? * , to JULIAR. *
 *grow yep * *shrink it's * *font=14 That * EASY!
-
+*hide *import matrix * *import security * *
+Hash anything with md5  *md5 yep *
+Generate Secure Password *password 16 *
 </juliar>
 <ijuliar></ijuliar>
